1d45c48657c9e4f37e0880e6a3b0d8a0d282cfe4
Szilveszter <nmszilveszter@gmail.com>
2025-07-10 15:39:11 +0200

    setup react app and npm packages

---
+++ frontend/catalog/vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg", "robots.txt", "apple-touch-icon.png"],
+      manifest: {
+        name: "Catalog",
+        short_name: "Catalog",
+        description: "Post as your cat. Rule as an overlord.",
+        theme_color: "#7C4DFF",
+        background_color: "#ffffff",
+        display: "standalone",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
